b26546413928759a3209ff677273df781ec7e05d
Calandiel <Calandiel@users.noreply.github.com>
2020-03-29 16:30:32 -0400

    Compatibility with 1.X tf

--- model.py
+++ model.py
@@ -8,11 +8,11 @@ def get_model():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Dense(g.INPUT_SIZE, activation='relu', input_dim=g.INPUT_SIZE),
         tf.keras.layers.Dense(g.INPUT_SIZE * 2, activation='relu'),
-        tf.keras.layers.Dropout(0.15),
+        tf.keras.layers.Dropout(rate=0.15),
         tf.keras.layers.Dense(g.INPUT_SIZE * 2, activation='relu'),
-        tf.keras.layers.Dropout(0.15),
+        tf.keras.layers.Dropout(rate=0.15),
         tf.keras.layers.Dense(g.INPUT_SIZE * 2, activation='relu'),
-        tf.keras.layers.Dropout(0.15),
+        tf.keras.layers.Dropout(rate=0.15),
         tf.keras.layers.Dense(g.OUTPUT_SIZE, activation='relu')
     ])
     checkpoint_path = os.path.join(pathlib.Path(__file__).parent.absolute(), "checkpoints")
