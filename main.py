ff73831cd41679c69f54571499f64b8da2ff70a1
Calandiel <Calandiel@users.noreply.github.com>
2021-11-01 07:02:40 +0100

    Finished data loading

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import sys
+import time
 from tensorflow.keras.preprocessing.image import load_img,img_to_array
 
 print('Python version: %s' % sys.version)
@@ -18,14 +19,84 @@ inps = os.listdir("./training_data_inputs")
 labels = os.listdir("./training_data_labels")
 data = set(inps) & set(labels)
 
+koppens = [
+	[255, 255, 255],
+	[0, 0, 255],
+	[0, 120, 255],
+	[70, 170, 250],
+	[255, 0, 0],
+	[255, 150, 150],
+	[245, 165, 0],
+	[255, 220, 100],
+	[255, 255, 0],
+	[200, 200, 0],
+	[150, 150, 0],
+	[150, 255, 150],
+	[100, 200, 100],
+	[50, 150, 50],
+	[200, 255, 80],
+	[100, 255, 80],
+	[50, 200, 0],
+	[255, 0, 255],
+	[200, 0, 200],
+	[150, 50, 150],
+	[170, 175, 255],
+	[89, 120, 220],
+	[75, 80, 179],
+	[0, 255, 255],
+	[55, 200, 255],
+	[0, 125, 125],
+	[178, 178, 178],
+	[102, 102, 102]
+]
+
 for a in data:
+	start_time = time.time()
+
 	img_input = img_to_array(load_img("./training_data_inputs/" + a, color_mode='rgb'))
 	img_label = img_to_array(load_img("./training_data_labels/" + a, color_mode='rgb'))
-	print(a)
-	print(type(img_input))
-	print(img_input[0, 0])
-	print(img_input.size)
-	print(img_input.shape)
+
+	input_data = np.zeros((img_input.shape[0], img_input.shape[1], 6))
+	label_data = np.zeros((img_input.shape[0], img_input.shape[1], 28))
+
+	for y in range(0, img_input.shape[0]):
+		for x in range(0, img_input.shape[1]):
+			# Process input
+			p = img_input[y, x]
+			if all(p == [0, 0, 255]):
+				input_data[y, x, 0] = 1 # sea
+			elif all(p == [177, 216, 230]):
+				input_data[y, x, 1] = 1 # shelf
+			elif all(p == [0, 0, 139]):
+				input_data[y, x, 2] # trench
+			elif all(p == [0, 255, 0]):
+				input_data[y, x, 3] # plains
+			elif all(p == [150, 75, 0]):
+				input_data[y, x, 4] # mountains
+			elif all(p == [112, 128, 144]):
+				input_data[y, x, 5] # tall mountains
+			else:
+				raise Exception("UNKNOWN INPUT COLOR IN : " + a) # unknown
+			# Process label
+			l = img_label[y, x]
+			min_dist = 255 * 4
+			index = 0
+			for n in range(0, len(koppens)):
+				h = koppens[n]
+				dist = abs(h[0] - l[0]) + abs(h[1] - l[1]) + abs(h[2] - l[2])
+				if dist < min_dist:
+					min_dist = dist
+					index = n
+					if dist < 5:
+						break
+			if min_dist > 5:
+				raise Exception("NO PIXEL SEEMS TO BE A CLOSE FIT FOR PIXEL: " + str(x) + ", " + str(y) + " IN: " + str(a) + " WITH COLOR: " + str(l))
+			label_data[y, x, index] = 1
+
+	end_time = time.time()
+	print(str(a) + ": " + str(end_time - start_time) + "s")
+
+print("Image loaded!")
 
 
 
